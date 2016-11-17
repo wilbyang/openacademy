7b4b13a96158da55aa8c2802835b7fa433dfd6ce
YangBo <yang.wilby@gmail.com>
2016-11-17 10:54:14 +0100

    first commit

---
+++ bizmodels.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'sth.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+    # relation
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'sth.session', 'course_id', string="Sessions")
+
+class Session(models.Model):
+    _name = 'sth.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+
+    # relation
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('sth.course',
+        ondelete='cascade', string="Course", required=True)
+
+    #attendee_ids = fields.Many2many('res.partner', string="Attendees")
